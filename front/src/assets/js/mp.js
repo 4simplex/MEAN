636066662c652d9e8f1b004ec70525640ec12ebb
EZEQUIEL <ezequielmansilla2.0@gmail.com>
2019-03-19 19:47:36 -0300

    Validations

--- front/src/assets/js/mp.js
+++ front/src/assets/js/mp.js
@@ -1,48 +1,132 @@
-Mercadopago.setPublishableKey("TEST-afa7b1b1-3d09-4f03-8628-12144ad07d10");
-console.log(Mercadopago)
+Mercadopago.setPublishableKey("TEST-ba92d932-3b5c-46c4-9b32-097c5edfe7da");
+console.log(Mercadopago);
+/*
+$.get("https://api.mercadopago.com/v1/payments/18196278?access_token=TEST-4038437526022988-100215-a0ccbdb999d932aa1c452bb1eb548bea__LA_LB__-24703927",function(data){
+    console.log(data)
+})
+*/
 function guessingPaymentMethod(event) {
-        var bin = event.target.value;
+    var bin = event.target.value;
 
-        if (event.type == "keyup") {
+    if (event.type == "keyup") {
+        if (bin.length >= 6) {
+            Mercadopago.getPaymentMethod({
+                "bin": bin
+            }, setPaymentMethodInfo);
+        }
+    } else {
+        setTimeout(function() {
             if (bin.length >= 6) {
                 Mercadopago.getPaymentMethod({
                     "bin": bin
                 }, setPaymentMethodInfo);
             }
-        } else {
-            setTimeout(function() {
-                if (bin.length >= 6) {
-                    Mercadopago.getPaymentMethod({
-                        "bin": bin
-                    }, setPaymentMethodInfo);
-                }
-            }, 100);
-        }
-    };
-var setPaymentMethodInfo = (status, response) => {
-        if (status == 200) {
-            document.querySelector("input[name=paymentMethodId]").value = response[0].id;
-        }
+        }, 100);
     }
+};
+
+var setPaymentMethodInfo = (status, response) => {
+    if (status == 200) {
+        document.querySelector("input[name=paymentMethodId]").value = response[0].id;
+    }
+}
 
 function sendPlan(payment_data) {
     fetch("http://localhost:3000/api/mp", {
-         method: 'POST',
-         body: JSON.stringify(payment_data),
-         headers:{
-             'Content-Type': 'application/json'
+            method: 'POST',
+            body: JSON.stringify(payment_data),
+            headers:{
+                'Content-Type': 'application/json'
             }
         })
         .then(function(response) {
-            console.log(response);
+            return response.json();
+        })
+        .then(function(response){
+            planMessages (response)
         })
 }
 
+function planMessages (data) {
+    console.log(data.status);
+    if(data.status.status === 201){
+        if(data.status.response.status === "approved" && data.status.response.status_detail === "accredited"){
+            alert("Listo, se acreditó tu pago! En tu resumen verás el cargo de amount como statement_descriptor.")
+        }
+
+        if(data.status.response.status === "in_process" && data.status.response.status_detail === "pending_contingency"){
+            alert("Estamos procesando el pago. En menos de 2 días hábiles te enviaremos por e-mail el resultado.")
+        }
+
+        if(data.status.response.status === "in_process" && data.status.response.status_detail === "pending_review_manual"){
+            alert("Estamos procesando el pago. En menos de 2 días hábiles te diremos por e-mail si se acreditó o si necesitamos más información.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_bad_filled_card_number"){
+            alert("Revisa el número de tarjeta.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_bad_filled_date"){
+            alert("Revisa la fecha de vencimiento.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_bad_filled_other"){
+            alert("Revisa los datos.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_bad_filled_security_code"){
+            alert("Revisa el código de seguridad.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_blacklist"){
+            alert("No pudimos procesar tu pago.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_call_for_authorize"){
+            alert("Debes autorizar ante " + data.status.response.payment_method_id + " el pago de amount a Mercado Pago.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_card_disabled"){
+            alert("Llama a " + data.status.response.payment_method_id + " para que active tu tarjeta. El teléfono está al dorso de tu tarjeta.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_card_error"){
+            alert("No pudimos procesar tu pago.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_duplicated_payment"){
+            alert("Ya hiciste un pago por ese valor. Si necesitas volver a pagar usa otra tarjeta u otro medio de pago.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_high_risk"){
+            alert("Tu pago fue rechazado. Elige otro de los medios de pago, te recomendamos con medios en efectivo.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_insufficient_amount"){
+            alert("Tu " + data.status.response.payment_method_id + " no tiene fondos suficientes.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_invalid_installments"){
+            alert(data.status.response.payment_method_id + "  no procesa pagos en " + data.status.response.installments + " installments cuotas.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_max_attempts"){
+            alert("Llegaste al límite de intentos permitidos. Elige otra tarjeta u otro medio de pago.")
+        }
+
+        if(data.status.response.status === "rejected" && data.status.response.status_detail === "cc_rejected_other_reason"){
+            alert(data.status.response.payment_method_id + "  no procesó el pago.")
+        }
+
+    } else {
+        alert("Ocurrio un problema")
+    }
+}
+
+
+
 $(document).ready(function() {
     Mercadopago.getIdentificationTypes();
-    $.get("https://api.mercadopago.com/v1/payments/18196278?access_token=TEST-4038437526022988-100215-a0ccbdb999d932aa1c452bb1eb548bea__LA_LB__-24703927",function(data){
-        console.log(data)
-    })
     
 
     let buttonsRadio = $('input[name=plan]');
@@ -59,15 +143,129 @@ $(document).ready(function() {
     
     $("#pay").submit(function( event ) {
         var $form = $(this);
-        Mercadopago.createToken($form, mpResponseHandler);
+        if(mpFormValNull($form)){
+            console.log($form);
+            Mercadopago.createToken($form, mpResponseHandler);
+        }
         event.preventDefault();
         return false;
     });
 
+    $("input, select").not(':input[type=submit], :input[type=hidden]').click(function(){
+        $(this).siblings("p").addClass("d-none");
+    })
+
+    var mpFormValNull = (form) => {
+        var isNotEmpty = true;
+       form.find("input, select").not(':input[type=submit], :input[type=hidden]').each(function(index, item){
+           if($(item).attr('id') === "docType"){
+                if($(item).find('option:selected').text() === ''){
+                    $(item).siblings(".emptyErr").removeClass("d-none");
+                    isNotEmpty = false;
+                    return;
+                }
+           }else{
+            if($(item).val() === ''){
+                $(item).siblings(".emptyErr").removeClass("d-none");
+                isNotEmpty = false;
+            }
+        }
+       })
+
+       return isNotEmpty;
+    } 
+
     var mpResponseHandler = function(status, response) {
         var $form = $('#pay');
         if (response.error) {
-            console.log(response);
+
+            $.each(response.cause, function(index, item){
+                switch(item.code) {
+                    case "205": 
+                        $("#cardNull").removeClass('d-none')
+                        break;
+                    case "208": 
+                        $("#writeMonth").removeClass('d-none')
+                    break;
+                    case "209": 
+                        $("#writeYear").removeClass('d-none')
+                    break;
+                    case "212": 
+                        $("#writeDocType").removeClass('d-none')
+                    break;
+                    case "213": 
+                        $("#writeSubType").removeClass('d-none') //no se que es
+                    break;
+                    case "214": 
+                        $("#writeDocNumber").removeClass('d-none')
+                    break;
+                    case "220": 
+                        $("#writeBank").removeClass('d-none') //banco emisor
+                    break;
+                    case "221": 
+                        $("#writeName").removeClass('d-none')
+                    break;
+                    case "224": 
+                        $("#writeDocCode").removeClass('d-none')
+                        break;
+                    case "316": 
+                        $("#validName").removeClass('d-none')
+                        break;
+                    case "322": 
+                        $("#validDocType").removeClass('d-none')
+                        break;
+                    case "323": 
+                        $("#validSubType").removeClass('d-none') //no se que es
+                        break;
+                    case "325": 
+                        $("#monthNotValid").removeClass('d-none')
+                        break;
+                    case "326": 
+                        $("#yearNotValid").removeClass('d-none')
+                        break;
+                    case "E301": 
+                        $("#cardNotValid").removeClass('d-none')
+                        break;
+                    case "E302": 
+                        $("#codeNotValid").removeClass('d-none')
+                        break;
+                    case "324": 
+                        $("#docNotValid").removeClass('d-none')
+                        break;
+                    case "106": 
+                        alert("No puedes realizar pagos a usuarios de otros países.") //generacion de token desde aqui
+                        break;
+                    case "109": 
+                        alert("payment_method_id no procesa pagos en installments cuotas. Elige otra tarjeta u otro medio de pago.") //ver variables
+                        break;
+                    case "126": 
+                        alert("No pudimos procesar tu pago.")
+                        break;
+                    case "129": 
+                        alert("payment_method_id no procesa pagos del monto seleccionado. Elige otra tarjeta u otro medio de pago.")
+                        break;
+                    case "145": 
+                        alert("No pudimos procesar tu pago.")
+                        break;
+                    case "150": 
+                        alert("No puedes realizar pagos.")
+                        break;
+                    case "151": 
+                        alert("No puedes realizar pagos.")
+                        break;
+                    case "160": 
+                        alert("No pudimos pocesar tu pago.")
+                        break;
+                    case "204": 
+                        alert("payment_method_id no está disponible en este momento. Elige otra tarjeta u otro medio de pago.")
+                        break;
+                    case "801": 
+                        alert("Realizaste un pago similar hace instantes. Intenta nuevamente en unos minutos.")
+                        break;
+                    default: 'Hubo un problema, ponte en contacto con OkayFunds!'
+                }
+            })
+            
         } else {
             var card_token_id = response.id;
             $form.append($('<input type="hidden" id="card_token_id" name="card_token_id"/>').val(card_token_id));
@@ -101,7 +299,7 @@ $(document).ready(function() {
                 };
                 sendPlan(advanced_data);
             }else{
-                alert("El plan no es valido");
+                alert("El plan no es valido, debera recargar la pagina");
                 return;
             }
 
--- server/controllers/mp.controller.js
+++ server/controllers/mp.controller.js
@@ -3,7 +3,7 @@ const mpCtrl = {};
 var mercadopago = require('mercadopago');
 
 mpCtrl.createPayment = (req, res) => {
-    mercadopago.configurations.setAccessToken("TEST-4038437526022988-100215-a0ccbdb999d932aa1c452bb1eb548bea__LA_LB__-24703927");
+    mercadopago.configurations.setAccessToken("TEST-4038437526022988-031819-fbb8c0a1f1dbdbd5db6af3b3b8daa0bf-24703927");
 
     var payment_data = {
     transaction_amount: req.body.transaction_amount,
@@ -12,7 +12,7 @@ mpCtrl.createPayment = (req, res) => {
     installments: 1,
     payment_method_id: req.body.payment_method_id,
     payer: {
-        email: 'ezequielmansilla2.0@gmail.com'
+        email: "ezequielmansilla2.0@gmail.com"
     }
     };
     // Save and posting the payment
@@ -20,12 +20,34 @@ mpCtrl.createPayment = (req, res) => {
     // ...    
     // Print the payment status
     console.log(data.body.id);
-
-     return res.json({algo})
+    
+     return res.json({status: data})
     })
     .catch(function (error) {
         return res.json({status: error})
     });
 }
+
+mpCtrl.registerUserAndCard = (data, token) => {
+    /*customer_data = { "email": data.body.payer.email }
+
+    mercadopago.customers.create(customer_data).then(function (customer) {
+
+    card_data = {
+        "token": token,
+        "customer": customer.id
+    }
+
+    mercadopago.cards.create(card_data).then(function (card) {
+
+        return res.json({status: card})
+    }).catch(function (error) {
+        return res.json({status: error})
+    });
+
+    }).catch(function (error) {
+        return res.json({status: error})
+    });*/
+};
 
 module.exports = mpCtrl;
